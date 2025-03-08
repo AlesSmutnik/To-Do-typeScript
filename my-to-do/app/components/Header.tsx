41642dcb0c52c14ef0322b53f4d349f4829ee87a
Aleš Smutník <a.smutnik@gmail.com>
2025-03-08 16:50:13 +0100

    last 16:50

--- my-to-do/app/components/Header.tsx
+++ my-to-do/app/components/Header.tsx
@@ -1,9 +1,10 @@
-import "../page.module.scss"
+import styles from "../page.module.scss";
+
 export default function Header () {
     return ( 
      <header className="all-h" >
-        <h1> trénik Typescript app To Do </h1>
-              <h2>seznam úkolů</h2>
+        <h1 className={styles.h1}> trénik Typescript app To Do </h1>
+              <h2 className={styles.h2}>seznam úkolů</h2>
      </header >
     );
 }
--- my-to-do/app/layout.tsx
+++ my-to-do/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header" 
 import Footer from "./components/Footer"
-import "./globals.css"
+
+
 
 export default function RootLayout({ children }: {children: React.ReactNode}) {
   return (
